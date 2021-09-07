f6d92872c0cbf19f61785aa53f66d4de78a4f962
Никита Байбородин <boratenot@gmail.com>
2021-09-07 16:58:42 +0800

    Code style changes.

--- Assets/Scripts/AxisTyped.cs
+++ Assets/Scripts/AxisTyped.cs
@@ -1,8 +1,8 @@
 ﻿public static class AxisTyped
 {
-    private static string _horrizontal = "Horizontal";
+    private static string _horizontal = "Horizontal";
     private static string _vertical = "Vertical";
 
-    public static string Horizontal => _horrizontal;
+    public static string Horizontal => _horizontal;
     public static string Vertical => _vertical;
 }
